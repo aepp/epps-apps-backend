936a2af379d3483eebe1ce4bc4df4c452fc7a378
Aleksandr Epp <aleksandr.epp@gmail.com>
2019-07-22 19:23:21 +0200

    Copy graphql scheme and config to deployment dist

--- tasks/bash/deploy_helper_production.sh
+++ tasks/bash/deploy_helper_production.sh
@@ -9,5 +9,6 @@ ssh-keyscan -H ${DEPLOY_SERVER_URL} >> ~/.ssh/known_hosts
 ssh deploy@${DEPLOY_SERVER_URL} "mkdir ${APP_ROOT}/temp_be"
 rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress \
     dist_server .dockerignore .env docker-cmd.sh docker-compose.yml Dockerfile package.json \
+    graphql.config.json graphql.schema.json \
     deploy@epps-apps.com:${APP_ROOT}/temp_be
 ssh deploy@${DEPLOY_SERVER_URL} "mkdir -p $APP_ROOT/releases_be/$RELEASE_NAME; mv $APP_ROOT/temp_be/* $APP_ROOT/releases_be/$RELEASE_NAME; rm -rf $APP_ROOT/temp_be; rsync -a -v $APP_ROOT/releases_be/$RELEASE_NAME/* $APP_ROOT/root/; cd $APP_ROOT/releases_be && ls -1tr | head -n -$KEEP_RELEASES | xargs -d '\n' rm -rf --; cd $APP_ROOT/root && docker-compose up -d --build --force-recreate"
