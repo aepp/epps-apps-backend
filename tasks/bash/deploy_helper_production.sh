7498af56ebd3fa396f2e6e578cc045e0f63bd4f5
Aleksandr Epp <aleksandr.epp@gmail.com>
2019-07-22 18:55:28 +0200

    Fix deployment helper script

--- tasks/bash/deploy_helper_production.sh
+++ tasks/bash/deploy_helper_production.sh
@@ -7,8 +7,7 @@ DEPLOY_SERVER_URL=epps-apps.com
 
 ssh-keyscan -H ${DEPLOY_SERVER_URL} >> ~/.ssh/known_hosts
 ssh deploy@${DEPLOY_SERVER_URL} "mkdir ${APP_ROOT}/temp_be"
-rsync "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" \
-    -avz -e --progress \
+rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress \
     dist_server .dockerignore .env docker-cmd.sh docker-compose.yml Dockerfile package.json \
     deploy@epps-apps.com:${APP_ROOT}/temp_be
 ssh deploy@${DEPLOY_SERVER_URL} "mkdir -p $APP_ROOT/releases_be/$RELEASE_NAME; mv $APP_ROOT/temp_be/* $APP_ROOT/releases_be/$RELEASE_NAME; rm -rf $APP_ROOT/temp_be; rsync -a -v $APP_ROOT/releases_be/$RELEASE_NAME/* $APP_ROOT/root/; cd $APP_ROOT/releases_be && ls -1tr | head -n -$KEEP_RELEASES | xargs -d '\n' rm -rf --; cd $APP_ROOT/root && docker-compose up -d --build --force-recreate"
