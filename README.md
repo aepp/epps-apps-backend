e965a3765fda9606bb685b4595db364d3d08df0d
Aleksandr Epp <aleksandr.epp@gmail.com>
2019-07-22 20:02:53 +0200

    Add build status badge; use multiline ssh command in deployment helper;

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# epps-apps-backend
+[![Build Status](https://semaphoreci.com/api/v1/aepp/epps-apps-backend/branches/master/badge.svg)](https://semaphoreci.com/aepp/epps-apps-backend)
+
+# Backend code for [https://epps-apps.com](https://epps-apps.com)
+
+- written in [TypeScript](https://www.typescriptlang.org/)
+- built with [Express](https://expressjs.com/)
+- bundled with [Webpack](https://webpack.js.org/)
--- tasks/bash/deploy_helper_production.sh
+++ tasks/bash/deploy_helper_production.sh
@@ -4,11 +4,24 @@ KEEP_RELEASES=3
 APP_ROOT=/usr/docker/containers/epps-apps.com
 RELEASE_NAME=$(date '+%Y_%m_%d_%H_%M_%S')
 DEPLOY_SERVER_URL=epps-apps.com
+TEMP_DIR=temp_be
+RELEASES_DIR=releases_be
 
 ssh-keyscan -H ${DEPLOY_SERVER_URL} >> ~/.ssh/known_hosts
-ssh deploy@${DEPLOY_SERVER_URL} "mkdir ${APP_ROOT}/temp_be"
+
+ssh deploy@${DEPLOY_SERVER_URL} "mkdir ${APP_ROOT}/${TEMP_DIR}"
+
 rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress \
     dist_server .dockerignore .env docker-cmd.sh docker-compose.yml Dockerfile package.json \
     graphql.config.json graphql.schema.json \
-    deploy@epps-apps.com:${APP_ROOT}/temp_be
-ssh deploy@${DEPLOY_SERVER_URL} "mkdir -p $APP_ROOT/releases_be/$RELEASE_NAME; mv $APP_ROOT/temp_be/* $APP_ROOT/releases_be/$RELEASE_NAME; rm -rf $APP_ROOT/temp_be; rsync -a -v $APP_ROOT/releases_be/$RELEASE_NAME/* $APP_ROOT/root/; cd $APP_ROOT/releases_be && ls -1tr | head -n -$KEEP_RELEASES | xargs -d '\n' rm -rf --; cd $APP_ROOT/root && docker-compose up -d --build --force-recreate"
+    deploy@epps-apps.com:${APP_ROOT}/${TEMP_DIR}
+
+ssh deploy@${DEPLOY_SERVER_URL} "\
+    mkdir -p $APP_ROOT/${RELEASES_DIR}/$RELEASE_NAME; \
+    mv $APP_ROOT/${TEMP_DIR}/* $APP_ROOT/${RELEASES_DIR}/$RELEASE_NAME; \
+    rm -rf $APP_ROOT/${TEMP_DIR}; \
+    rsync -a -v $APP_ROOT/${RELEASES_DIR}/$RELEASE_NAME/* $APP_ROOT/root/; \
+    cd $APP_ROOT/${RELEASES_DIR} && ls -1tr | head -n -$KEEP_RELEASES | xargs -d '\n' rm -rf --; \
+    cd $APP_ROOT/root && docker-compose up -d --build --force-recreate;\
+    rm -rf ${APP_ROOT}/${TEMP_DIR}
+"
